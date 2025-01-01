f8872b41c3b6a975a7218c32aaf9fd5f470613db
Alex <alexander.pieroni@duke.edu>
2025-01-01 23:29:34 +0100

    edit to 15 mins

--- main.py
+++ main.py
@@ -30,10 +30,10 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
 logger = logging.getLogger()
 
 priority_to_time_block = {
-    "Low": 30,
-    "Medium": 30,
-    "High": 30,
-    "Must Be Done Today": 30
+    "Low": 15,
+    "Medium": 15,
+    "High": 15,
+    "Must Be Done Today": 15
 }
 
 headers = {
